910629478656cb762ac2593b7e70125937d6580d
Christoffer <christofferalves10@gmail.com>
2025-04-04 11:42:06 -0300

    Adicionando uma comparação top

--- src/js/script.js
+++ src/js/script.js
@@ -40,33 +40,58 @@
 //CONVERSÕES
 
 //FLOAT => STRING
-let numfloat = 123.456;
-console.log(numfloat.toString())
+// let numfloat = 123.456;
+// console.log(numfloat.toString())
 
 // STRING => FLOAT
-let numString = "12.963"
-console.log(parseFloat(numString))
+// let numString = "12.963"
+// console.log(parseFloat(numString))
 
 // INT => STRING
-let numInt = 10;
-console.log (numInt.toString())
+// let numInt = 10;
+// console.log (numInt.toString())
 
 // STRING => INT
-let numstring = "29"
-console.log (parseFloat(numstring))
+// let numstring = "29"
+// console.log (parseFloat(numstring))
 
 // MÉTODOS
 
 // MÉTODO LENGTH - VERIFICA O TAMANHO DA STRING
-let frase = "O mundo da tecnologia"
-console.log(frase.length)
+// let frase = "O mundo da tecnologia"
+// console.log(frase.length)
 
 // MÉTODO indexOf/lastIndexOf - Retorna um trecho do seu código
-let texto = "Programação sustentável";
-console.log (texto.lastIndexOf("ão"))
+// let texto = "Programação sustentável";
+// console.log (texto.lastIndexOf("ão"))
 
 // MÉTODO slice - retorna parte de um texto, passando o ínicio e o final.
-let info ="Programação de ponta";
-console.log(info.slice(2,6))
+// let info ="Programação de ponta";
+// console.log(info.slice(2,6))
 
 // OPERADORES ARITMÉTICOS
+// const a =10;
+// const b=20;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+
+// OPERADORES LÓGICOS 
+// const c=20;
+// const d =30;
+
+// console.log(c < d);
+// console.log(c > d && c < 10);
+// console.log(c > d || d > c);
+// console.log (c == d || d <= c);
+
+
+// OPERADORES DE COMPARAÇÃO
+// const e = 10;
+// const f = 30;
+
+// console.log (e == f);
+// console.log (e === f);
+// console.log (e != f);
